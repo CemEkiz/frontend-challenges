f7a378b2398647daae0fba0d05fa2a10640e1841
Ekiz Cem <ekizcem.pro@gmail.com>
2022-07-26 23:01:49 +0200

    Implementing the Header and the Mobile Navbar

--- e-commerce-product-page/script.js
+++ e-commerce-product-page/script.js
@@ -1,1 +1,20 @@
+const btnClose = document.querySelector('.nav__btn-close');
+const btnOpen = document.querySelector('.header__btn-open');
+const navMenuMobile = document.querySelector('.nav');
+const overlay = document.querySelector('.overlay');
+const navLinks = document.querySelectorAll('.nav__list__link');
 
+const openMenu = () => {
+	navMenuMobile.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
+
+const closeMenu = () => {
+	navMenuMobile.classList.add('hidden');
+	overlay.classList.add('hidden');
+};
+
+btnOpen.addEventListener('click', openMenu);
+btnClose.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+navLinks.forEach((link) => link.addEventListener('click', closeMenu));
