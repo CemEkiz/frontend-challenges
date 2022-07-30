d6f5bc58b98062b2b911e6335b765d71bb4bec05
Ekiz Cem <ekizcem.pro@gmail.com>
2022-07-30 20:18:40 +0200

    implementing the price logic, update UI, notif, cart and calculate price

--- js/script.js
+++ js/script.js
@@ -1,10 +1,39 @@
+'use strict';
+
+////////////////// HTML Elements //////////////////
+
 const btnClose = document.querySelector('.nav__btn-close');
 const btnOpen = document.querySelector('.header__btn-open');
 const navMenuMobile = document.querySelector('.nav');
 const overlay = document.querySelector('.overlay');
 const navLinks = document.querySelectorAll('.nav__list__link');
+const btnMinus = document.querySelector('.item__btn-minus');
+const btnPlus = document.querySelector('.item__btn-plus');
+const itemNb = document.querySelector('.item__number');
+const notif = document.querySelector('.header__cart-icon--notif');
+const shopIcon = document.querySelector('.header__cart-icon');
+const btnAdd = document.querySelector('.btn-add');
+const cart = document.querySelector('.cart');
+const cartProduct = document.querySelector('.cart__product');
+const cartEmpty = document.querySelector('.cart__empty');
+const cartPrice = document.querySelector('.cart__price');
+const priceContainer = document.querySelector('.product-info__price');
+const deleteProduct = document.querySelector('.cart__product__delete');
 
-// Nav Mobile
+////////////////// Reusable Functions //////////////////
+
+// Hide Element
+const hideEl = (element) => {
+	element.classList.add('hidden', 'hidden-cart');
+};
+
+// Show Element
+const displayEl = function (element) {
+	element.classList.remove('hidden', 'hidden-cart');
+};
+
+////////////////// Nav Mobile //////////////////
+
 const openMenu = () => {
 	navMenuMobile.classList.remove('hidden');
 	overlay.classList.remove('hidden');
@@ -20,7 +49,7 @@ btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 navLinks.forEach((link) => link.addEventListener('click', closeMenu));
 
-// Slider
+////////////////// Slider //////////////////
 
 var swiper = new Swiper('.mySwiper', {
 	slidesPerView: 1,
@@ -37,24 +66,92 @@ var swiper = new Swiper('.mySwiper', {
 	},
 });
 
-///////////// Calculate Shopping Cart ///////////////
-const btnMinus = document.querySelector('.item__btn-minus');
-const btnPlus = document.querySelector('.item__btn-plus');
-const numberItem = document.querySelector('.item__number');
-
-numberCalc = 0;
-
-// Increase number of item by 1
-btnPlus.addEventListener('click', function () {
-	numberCalc++;
-	numberItem.textContent = numberCalc;
+////////////////// Show Shopping Cart //////////////////
+shopIcon.addEventListener('click', function () {
+	if (cart.classList.contains('hidden')) {
+		displayEl(cart);
+	} else {
+		hideEl(cart);
+	}
 });
 
-// Decrease number of item by 1
-btnMinus.addEventListener('click', function () {
-	// Guard Clause
-	if (numberCalc === 0) return;
+////////////////// Price //////////////////
 
-	numberCalc--;
-	numberItem.textContent = numberCalc;
-});
+const definePrice = (priceBefore, discount) => {
+	priceContainer.innerHTML = '';
+
+	const priceAfter = priceBefore * (discount / 100);
+
+	// Display UI ---> Description
+	const html = `
+		<div class="product-info__price__after">$${priceAfter.toFixed(2)}</div>
+		<span class="product-info__price__discount">${discount}%</span>
+		<div class="product-info__price__before">$${priceBefore.toFixed(2)}</div>
+	`;
+
+	priceContainer.insertAdjacentHTML('afterbegin', html);
+
+	// Update Notif
+	const updateNotif = () => {
+		if (itemCalc > 0) {
+			displayEl(notif);
+		} else {
+			hideEl(notif);
+		}
+	};
+
+	// Update Price (UI) ---> Cart
+	let itemCalc = Number(itemNb.textContent);
+	itemCalc = 0;
+
+	const updatePrice = () => {
+		cartPrice.innerHTML = '';
+
+		// Display UI --> Cart
+		const html = `
+		<span>$${priceAfter.toFixed(2)}</span> x <span>${itemCalc}</span>
+		<span class="cart__product__info-final-price">$${(
+			priceAfter * itemCalc
+		).toFixed(2)}</span>
+		`;
+
+		cartPrice.insertAdjacentHTML('afterbegin', html);
+
+		itemNb.textContent = itemCalc;
+		notif.textContent = itemCalc;
+
+		if (itemNb.textContent > 0) {
+			displayEl(notif);
+			displayEl(cartProduct);
+			hideEl(cartEmpty);
+		} else {
+			hideEl(notif);
+			displayEl(cartEmpty);
+			hideEl(cartProduct);
+		}
+	};
+
+	// Increase number of item by 1 and update UI
+	btnPlus.addEventListener('click', function () {
+		itemCalc++;
+		updatePrice();
+
+		updateNotif();
+
+		console.log(itemCalc);
+	});
+
+	// Decrease number of item by 1 and update UI
+	btnMinus.addEventListener('click', function () {
+		// Guard Clause
+		if (itemCalc === 0) return;
+		itemCalc--;
+		updatePrice();
+
+		updateNotif();
+
+		console.log(itemCalc);
+	});
+};
+
+definePrice(500, 50);
